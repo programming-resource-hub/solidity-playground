9a7e6751d8fdcb348d1ee83d49e653ffc97ad081
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:10:09 +0100

    tutorial-07

--- tutorials/tutorial-07/Strings.sol
+++ tutorials/tutorial-07/Strings.sol
@@ -1,49 +1,40 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.0;
 
 library Strings {
-    function concat(string memory _base, string memory _value)
-        internal
-        pure
-        returns (string memory)
-    {
+    
+    function concat(string _base, string _value) pure internal returns (string) {
         bytes memory _baseBytes = bytes(_base);
         bytes memory _valueBytes = bytes(_value);
-
-        string memory _tmpValue = new string(
-            _baseBytes.length + _valueBytes.length
-        );
+        
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
         bytes memory _newValue = bytes(_tmpValue);
-
-        uint256 i;
-        uint256 j;
-
-        for (i = 0; i < _baseBytes.length; i++) {
+        
+        uint i;
+        uint j;
+        
+        for(i=0;i<_baseBytes.length;i++) {
             _newValue[j++] = _baseBytes[i];
         }
-
-        for (i = 0; i < _valueBytes.length; i++) {
+        
+        for(i=0;i<_valueBytes.length;i++) {
             _newValue[j++] = _valueBytes[i];
         }
-
+        
         return string(_newValue);
     }
-
-    function strpos(string memory _base, string memory _value)
-        internal
-        pure
-        returns (int256)
-    {
+    
+    function strpos(string _base, string _value) pure internal returns (int) {
         bytes memory _baseBytes = bytes(_base);
         bytes memory _valueBytes = bytes(_value);
 
-        assert(_valueBytes.length == 1);
-
-        for (uint256 i = 0; i < _baseBytes.length; i++) {
+        assert(_valueBytes.length == 1);        
+        
+        for(uint i=0;i<_baseBytes.length;i++) {
             if (_baseBytes[i] == _valueBytes[0]) {
-                return int256(i);
+                return int(i);
             }
         }
-
+        
         return -1;
     }
 }
--- tutorials/tutorial-07/TestStrings.sol
+++ tutorials/tutorial-07/TestStrings.sol
@@ -1,15 +1,16 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.0;
 
-import "./Strings.sol";
+import "browser/Strings.sol";
 
 contract TestStrings {
+    
     using Strings for string;
-
+    
     function testConcat(string _base) public pure returns (string) {
         return _base.concat("_suffix");
     }
-
-    function needleInHaystack(string _base) public pure returns (int256) {
+    
+    function needleInHaystack(string _base) public pure returns (int) {
         return _base.strpos("t");
     }
 }
