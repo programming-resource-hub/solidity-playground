87a6b9c99c676f634348b9c4001b556e6c932bd6
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:05:49 +0100

    tutorial-03

--- tutorials/tutorial-03/myfirstcontract.sol
+++ tutorials/tutorial-03/myfirstcontract.sol
@@ -1,66 +1,65 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.0;
 
 interface Regulator {
-    function checkValue(uint256 amount) external returns (bool);
-
+    function checkValue(uint amount) external returns (bool);
     function loan() external returns (bool);
 }
 
 contract Bank is Regulator {
-    uint256 private value;
+    uint private value;
     address private owner;
-
-    modifier ownerFunc() {
+    
+    modifier ownerFunc {
         require(owner == msg.sender);
         _;
     }
 
-    constructor(uint256 amount) {
+    constructor(uint amount) public {
         value = amount;
         owner = msg.sender;
     }
-
-    function deposit(uint256 amount) public ownerFunc {
+    
+    function deposit(uint amount) public ownerFunc {
         value += amount;
     }
-
-    function withdraw(uint256 amount) public ownerFunc {
+    
+    function withdraw(uint amount) public ownerFunc {
         if (checkValue(amount)) {
             value -= amount;
         }
     }
-
-    function balance() public view returns (uint256) {
+    
+    function balance() public view returns (uint) {
         return value;
     }
-
-    function checkValue(uint256 amount) public view returns (bool) {
+    
+    function checkValue(uint amount) public returns (bool) {
         // Classic mistake in the tutorial value should be above the amount
         return value >= amount;
     }
-
-    function loan() public view returns (bool) {
+    
+    function loan() public returns (bool) {
         return value > 0;
     }
 }
 
-contract MyContract is Bank(10) {
+contract MyFirstContract is Bank(10) {
     string private name;
-    uint256 private age;
-
-    function setName(string memory newName) public {
+    uint private age;
+    
+    function setName(string newName) public {
         name = newName;
     }
-
-    function getName() public view returns (string memory) {
+    
+    function getName() public view returns (string) {
         return name;
     }
-
-    function setAge(uint256 newAge) public {
+    
+    function setAge(uint newAge) public {
         age = newAge;
     }
-
-    function getAge() public view returns (uint256) {
+    
+    function getAge() public view returns (uint) {
         return age;
     }
 }
@@ -69,17 +68,16 @@ contract TestThrows {
     function testAssert() public pure {
         assert(1 == 2);
     }
-
+    
     function testRequire() public pure {
         require(2 == 1);
     }
-
+    
     function testRevert() public pure {
         revert();
     }
-
-    // Deprecated
-    // function testThrow() public pure {
-    //     throw;
-    // }
+    
+    function testThrow() public pure {
+        throw;
+    }
 }
