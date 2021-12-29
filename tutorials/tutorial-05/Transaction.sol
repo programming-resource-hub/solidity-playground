a45ab4b20a3bb583ce4bab7cedbf5133d08b5c9d
devcraft1 <ukonudennis64@gmail.com>
2021-12-28 23:29:43 -0900

    tutorial-05

---
+++ tutorials/tutorial-05/Transaction.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.0;
+
+contract Transaction {
+    event SenderLogger(address);
+    event ValueLogger(uint256);
+
+    address private owner;
+
+    modifier isOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier validValue() {
+        assert(msg.value >= 1 ether);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function() public payable isOwner validValue {
+        emit SenderLogger(msg.sender);
+        emit ValueLogger(msg.value);
+    }
+}
