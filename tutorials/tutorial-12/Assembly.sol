4cf580c812116455ccee082b00c4d1c9ec1782aa
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:13:45 +0100

    tutorial-12

--- tutorials/tutorial-12/Assembly.sol
+++ tutorials/tutorial-12/Assembly.sol
@@ -1,38 +1,38 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.0;
 
 contract Assembly {
-    function nativeLoops() public returns (uint256 _r) {
-        for (uint256 i = 0; i < 10; i++) {
+    function nativeLoops() public returns (uint _r) {
+        for(uint i = 0; i < 10; i++) {
             _r++;
         }
     }
-
-    function asmLoops() public returns (uint256 _r) {
+    
+    function asmLoops() public returns (uint _r) {
         assembly {
             let i := 0
-        loop:
+            loop:
             i := add(i, 1)
             _r := add(_r, 1)
             jumpi(loop, lt(i, 10))
         }
     }
 
-    function nativeConditional(uint256 _v) public returns (uint256) {
+    function nativeConditional(uint _v) public returns (uint) {
         if (5 == _v) {
             return 55;
         } else if (6 == _v) {
             return 66;
-        }
+        } 
         return 11;
     }
-
-    function asmConditional(uint256 _v) public returns (uint256 _r) {
+    
+    function asmConditional(uint _v) public returns (uint _r) {
         assembly {
             switch _v
             case 5 {
                 _r := 55
             }
-            case 6 {
+            case 6 { 
                 _r := 66
             }
             default {
@@ -40,8 +40,8 @@ contract Assembly {
             }
         }
     }
-
-    function asmReturns(uint256 _v) public returns (uint256) {
+    
+    function asmReturns(uint _v) public returns (uint) {
         assembly {
             let _ptr := add(msize(), 1)
             mstore(_ptr, _v)
