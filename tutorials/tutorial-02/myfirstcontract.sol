759a5427f75fb9c60223bff1e2249e39f0bdbc00
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:05:08 +0100

    tutorial-02

--- tutorials/tutorial-02/myfirstcontract.sol
+++ tutorials/tutorial-02/myfirstcontract.sol
@@ -1,59 +1,58 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
 interface Regulator {
-    function checkValue(uint256 amount) external returns (bool);
-
+    function checkValue(uint amount) external returns (bool);
     function loan() external returns (bool);
 }
 
 contract Bank is Regulator {
-    uint256 private value;
-
-    constructor(uint256 amount) {
+    uint private value;
+    
+    constructor(uint amount) public {
         value = amount;
     }
-
-    function deposit(uint256 amount) public {
+    
+    function deposit(uint amount) public {
         value += amount;
     }
-
-    function withdraw(uint256 amount) public {
+    
+    function withdraw(uint amount) public {
         if (checkValue(amount)) {
             value -= amount;
         }
     }
-
-    function balance() public view returns (uint256) {
+    
+    function balance() public view returns (uint) {
         return value;
     }
-
-    function checkValue(uint256 amount) public view returns (bool) {
+    
+    function checkValue(uint amount) public returns (bool) {
         // Classic mistake in the tutorial value should be above the amount
         return value >= amount;
     }
-
-    function loan() public view returns (bool) {
+    
+    function loan() public returns (bool) {
         return value > 0;
     }
 }
 
-contract MyContract is Bank(10) {
+contract MyFirstContract is Bank(10) {
     string private name;
-    uint256 private age;
-
+    uint private age;
+    
     function setName(string memory newName) public {
         name = newName;
     }
-
+    
     function getName() public view returns (string memory) {
         return name;
     }
-
-    function setAge(uint256 newAge) public {
+    
+    function setAge(uint newAge) public {
         age = newAge;
     }
-
-    function getAge() public view returns (uint256) {
+    
+    function getAge() public view returns (uint) {
         return age;
     }
 }
