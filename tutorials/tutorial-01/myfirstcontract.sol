48495051d665c23d6b1485f225c55b20109518a8
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:04:23 +0100

    tutorial-01

--- tutorials/tutorial-01/myfirstcontract.sol
+++ tutorials/tutorial-01/myfirstcontract.sol
@@ -1,22 +1,22 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
-contract MyContract {
+contract MyFirstContract {
     string private name;
-    uint256 private age;
-
+    uint private age;
+    
     function setName(string memory newName) public {
         name = newName;
     }
-
+    
     function getName() public view returns (string memory) {
         return name;
     }
-
-    function setAge(uint256 newAge) public {
+    
+    function setAge(uint newAge) public {
         age = newAge;
     }
-
-    function getAge() public view returns (uint256) {
+    
+    function getAge() public view returns (uint) {
         return age;
     }
 }
