7aecb7df811042f0fd20355bf8a60136219e4b5d
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:15:50 +0100

    tutorial-16

--- tutorials/tutorial-16/TimeBased.sol
+++ tutorials/tutorial-16/TimeBased.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
 contract TimeBased {
-    mapping(address => uint256) public _balanceOf;
-    mapping(address => uint256) public _expiryOf;
-
-    uint256 private leaseTime = 600;
-
+    
+    mapping(address => uint) public _balanceOf;
+    mapping(address => uint) public _expiryOf;
+    
+    uint private leaseTime = 600;
+    
     modifier expire(address _addr) {
         if (_expiryOf[_addr] >= block.timestamp) {
             _expiryOf[_addr] = 0;
@@ -13,28 +14,42 @@ contract TimeBased {
         }
         _;
     }
-
-    function lease() public payable expire(msg.sender) returns (bool) {
+    
+    function lease()
+        public
+        payable
+        expire(msg.sender)
+        returns (bool) {
         require(msg.value == 1 ether);
         require(_balanceOf[msg.sender] == 0);
         _balanceOf[msg.sender] = 1;
         _expiryOf[msg.sender] = block.timestamp + leaseTime;
         return true;
     }
-
-    function balanceOf() public returns (uint256) {
-        return balanceOf(msg.sender);
+    
+    function balanceOf() 
+        public
+        returns (uint) {
+        return balanceOf(msg.sender);        
     }
-
-    function balanceOf(address _addr) public expire(_addr) returns (uint256) {
+    
+    function balanceOf(address _addr)
+        public
+        expire(_addr)
+        returns (uint) {
         return _balanceOf[_addr];
     }
-
-    function expiryOf() public returns (uint256) {
-        return expiryOf(msg.sender);
+    
+    function expiryOf() 
+        public
+        returns (uint) {
+        return expiryOf(msg.sender);        
     }
-
-    function expiryOf(address _addr) public expire(_addr) returns (uint256) {
+    
+    function expiryOf(address _addr)
+        public
+        expire(_addr)
+        returns (uint) {
         return _expiryOf[_addr];
     }
 }
