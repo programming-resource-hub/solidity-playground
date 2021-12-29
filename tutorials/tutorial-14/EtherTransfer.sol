cc0be2d98275b1f5ba85ffcdc0fb75b81fe645f8
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:14:48 +0100

    tutorial-14

--- tutorials/tutorial-14/EtherTransfer.sol
+++ tutorials/tutorial-14/EtherTransfer.sol
@@ -1,31 +1,33 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
 contract EtherTransferTo {
-    function() external payable {}
-
-    function getBalance() public returns (uint256) {
+    function () external payable {
+    }
+    
+    function getBalance() public returns (uint) {
         return address(this).balance;
     }
 }
 
 contract EtherTransferFrom {
+    
     EtherTransferTo private _instance;
-
+    
     constructor() public {
         // _instance = EtherTransferTo(address(this));
         _instance = new EtherTransferTo();
     }
-
-    function getBalance() public returns (uint256) {
+    
+    function getBalance() public returns (uint) {
         return address(this).balance;
     }
-
-    function getBalanceOfInstance() public returns (uint256) {
+    
+    function getBalanceOfInstance() public returns (uint) {
         //return address(_instance).balance;
         return _instance.getBalance();
     }
-
-    function() external payable {
+    
+    function () external payable {
         //msg.sender.send(msg.value);
         address(_instance).send(msg.value);
     }
