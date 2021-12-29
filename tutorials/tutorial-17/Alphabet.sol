1840877ecc4a2b4d4de667b7a9d6634b2da3b3b2
devcraft1 <ukonudennis64@gmail.com>
2021-12-29 16:16:17 +0100

    tutorial-17

--- tutorials/tutorial-17/Alphabet.sol
+++ tutorials/tutorial-17/Alphabet.sol
@@ -1,51 +1,59 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.0;
 
 interface Letter {
-    function n() external returns (uint256);
+    function n() public returns (uint);
 }
 
 contract A is Letter {
-    function n() public returns (uint256) {
-        return 1;
+    function n()
+        public
+        returns (uint) {
+        return 1;    
     }
 }
 
 contract B is A {}
 
 contract C is Letter {
-    function n() public pure returns (uint256) {
+    function n()
+        public
+        returns (uint) {
         return 2;
     }
-
-    function x() public pure returns (string memory) {
-        return "x";
+    
+    function x() 
+        public
+        returns (string) {
+        return "x";        
     }
 }
 
 contract Alphabet {
+    
     Letter[] private letters;
-
-    event Printer(uint256);
-
-    constructor() {
+    
+    event Printer(uint);
+    
+    function Alphabet()
+        public {
         letters.push(new A());
         letters.push(new B());
         letters.push(new C());
     }
-
-    function loadRemote(
-        address _addrX,
-        address _addrY,
-        address _addrZ
-    ) public {
+    
+    function loadRemote(address _addrX,
+                        address _addrY,
+                        address _addrZ)
+        public {
         letters.push(Letter(_addrX));
         letters.push(Letter(_addrY));
-        letters.push(Letter(_addrZ));
+        letters.push(Letter(_addrZ));    
     }
-
-    function printLetters() public {
-        for (uint256 i = 0; i < letters.length; i++) {
-            emit Printer(letters[i].n());
-        }
+    
+    function printLetters()
+        public {
+        for(uint i = 0; i < letters.length; i++) {
+            Printer(letters[i].n());
+        }    
     }
 }
