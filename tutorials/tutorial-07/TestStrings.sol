1bb62d27b8bb7a797358c8eec828265c1585f638
devcraft1 <ukonudennis64@gmail.com>
2021-12-30 23:05:19 -1000

    new

--- tutorials/tutorial-07/TestStrings.sol
+++ tutorials/tutorial-07/TestStrings.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.4.0;
 import "browser/Strings.sol";
 
 contract TestStrings {
-    
     using Strings for string;
-    
+
     function testConcat(string _base) public pure returns (string) {
         return _base.concat("_suffix");
     }
-    
-    function needleInHaystack(string _base) public pure returns (int) {
+
+    function needleInHaystack(string _base) public pure returns (int256) {
         return _base.strpos("t");
     }
 }
